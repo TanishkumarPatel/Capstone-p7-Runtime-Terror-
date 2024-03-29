7786f8bbcf3f8bc9bb3c9c43e6902b61d826dfa3
Tanishkumar Patel <202301411@daiict.in>
2024-03-29 12:38:55 +0530

    second commit

---
+++ inputnew.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
+int main()
+{
+   vector <string> inmate_name;
+    vector <string> earpod_id;
+    vector <pair<string,string>> sleep_time;
+    vector <string> time_taken_to_fall_asleep;
+    vector <string> dorm_name;
+    vector <string> music_channels_id;
+
+        ifstream input("inmate.txt");
+        string perticularline;
+        while(getline(input,perticularline))
+        {
+            istringstream iss(perticularline);
+            string name,earpod,sleeptime1,sleeptime2,asleeptime;
+            if(getline(iss,name,',') and getline(iss,earpod,',') and getline(iss,sleeptime1,',') and getline(iss,sleeptime2,',') and getline(iss,asleeptime,',') )
+            {
+                inmate_name.push_back(name);
+                earpod_id.push_back(earpod);
+                sleep_time.push_back({sleeptime1,sleeptime2});
+                time_taken_to_fall_asleep.push_back(asleeptime);
+            }
+        }
+         
+    
+        ifstream infile("dorm.txt");
+        string line;
+        while(getline(infile,line))
+        {
+            istringstream iss(line);
+            string name,channelid;
+            if(getline(iss,name,',') and getline(iss,channelid,',') )
+            {
+                dorm_name.push_back(name);
+                music_channels_id.push_back(channelid);
+            }
+        }
+
+        // cout<<inmate_name[2]<<" ";
+        // cout<<earpod_id[2]<<" ";
+        // cout<<sleep_time[2]<<" ";
+        // cout<<time_taken_to_fall_asleep[2]<<" "<<endl;
+        // cout<<dorm_name[5]<<" ";
+        // cout<<music_channels_id[5]<<" ";
+}
